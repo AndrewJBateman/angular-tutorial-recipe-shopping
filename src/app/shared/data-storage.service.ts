e75dc54f5971ac8454aeed48157d3308d2545666
Andrew Bateman <gomezbateman@yahoo.com>
2019-02-22 12:35:20 +0100

    HttpClient requests and interceptors added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -8,23 +8,33 @@ import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, 
-  private recipeService: RecipeService,
-  private authService: AuthService) {}
+  constructor(private httpClient: HttpClient, 
+    private recipeService: RecipeService,
+    private authService: AuthService) {}
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.http.put('https://ng-recipe-book-a2167.firebaseio.com/recipes.json?auth=' +token, 
-      this.recipeService.getRecipes());
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer dfsfsfsfis');
+    // return this.httpClient.put('https://ng-recipe-book-a2167.firebaseio.com/recipes.json?auth=' +token, 
+    //   this.recipeService.getRecipes(), {
+    //     observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //     // headers: headers
+    //   });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-a2167.firebaseio.com/recipes.json', 
+      this.recipeService.getRecipes(), {reportProgress: true});
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
-    const token = this.authService.getToken();
  
-    this.http.get('https://ng-recipe-book-a2167.firebaseio.com/recipes.json?auth=' +token)
+    // this.httpClient.get<Recipe[]>('https://ng-recipe-book-a2167.firebaseio.com/recipes.json?auth=' +token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-a2167.firebaseio.com/recipes.json', {
+      observe: 'body',
+      responseType: 'json'
+    })
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
+          console.log(recipes);
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
