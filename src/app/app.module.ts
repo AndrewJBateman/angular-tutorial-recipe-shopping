82844d1d985761a421c582b19eb627e2e6c925b9
Andrew Bateman <gomezbateman@yahoo.com>
2019-02-26 12:16:17 +0100

    code corrected & nodejs server file generated. Fully working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { trigger, state, style } from '@angular/animations';  
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
@@ -23,6 +25,7 @@ import { AuthEffects } from './auth/store/auth.effects';
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
